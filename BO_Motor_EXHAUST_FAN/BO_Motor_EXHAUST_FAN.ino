1067523fe625f3450109a7e9ba8ae468a1e0f0b3
ANTO B <bonifonantob@gmail.com>
2026-06-24 11:31:29 +0530

    Add files via upload

---
+++ BO_Motor_EXHAUST_FAN/BO_Motor_EXHAUST_FAN.ino
@@ -0,0 +1,17 @@
+int IN1 = 8;
+int IN2 = 9;
+void setup()
+{
+  pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT);
+}
+void loop()
+{
+  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  delay(3000);
+  digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
+  delay(1000);
+  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
+  delay(3000);
+  digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
+  delay(1000);
+}
